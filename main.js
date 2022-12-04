3902bb215676d93afacb42305b357e58662e7064
Caio Leme <caioacleme@gmail.com>
2022-12-04 16:31:08 -0300

    Deletado servidor local e adicionado axios via CDN
    Deletado servidor local para executar o projeto via liveserver e adicionado axios via CDN para executar promisses

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-import axios from 'axios'
 import { API_KEY, BASE_URL, IMG_URL, language } from './api.js'
 
 //const movieImage = document.getElementById('movieImage')
@@ -8,12 +7,11 @@ const movieId = Math.floor(Math.random() * 1000 + 1)
 
 //https://api.themoviedb.org/3/movie/550?api_key=f6fc39d1a09cfead5379199d398c2281
 const url = `${BASE_URL}${movieId}?api_key=${API_KEY}&${language}`
-//const url = `https://localhost:3000`
 
 console.log(movieId)
 console.log(url)
 
-function findMovie() {
+function changeMovie() {
   axios
     .get(url)
     .then(response => {
