5b0e189d75006fbe3c28d5a7aed9638163f8a6ad
Caio Leme <caioacleme@gmail.com>
2022-12-04 13:23:16 -0300

    Testando o site no githubpages
    Testando o site no githubpages

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-import {
-  API_KEY, BASE_URL,
-  IMG_URL,
-  language,
-} from './api.js'
+import { API_KEY, BASE_URL, IMG_URL, language } from './api.js'
+
+//const movieImage = document.getElementById('movieImage')
+//const movieTitle = document.getElementById('movieTitle')
+//const movieDescription = document.getElementById('movieDescription')
+const movieId = Math.floor(Math.random() * 1000 + 1)
+
+//https://api.themoviedb.org/3/movie/550?api_key=f6fc39d1a09cfead5379199d398c2281
+const url = `${BASE_URL}${movieId}?api_key=${API_KEY}&${language}`
+
+console.log(movieId)
+console.log(url)
