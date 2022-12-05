057f065667ea6a197d9ccd47a14b2c0726aa25cc
Caio Leme <caioacleme@gmail.com>
2022-12-05 19:27:45 -0300

    Adicionado link com api do moviedb
    Adicionado link com api do moviedb e comandos para trocar o conteúdo do HTML.

--- main.js
+++ main.js
@@ -1,21 +1,34 @@
 import { API_KEY, BASE_URL, IMG_URL, language } from './api.js'
 
-//const movieImage = document.getElementById('movieImage')
-//const movieTitle = document.getElementById('movieTitle')
-//const movieDescription = document.getElementById('movieDescription')
-const movieId = Math.floor(Math.random() * 1000 + 1)
+const movieImage = document.getElementById('movieImage')
+const movieTitle = document.getElementById('movieTitle')
+const movieSummary = document.getElementById('movieSummary')
+const findMovie = document.getElementById('findMovie')
+findMovie.addEventListener('click', findMovieFunc)
 
 //https://api.themoviedb.org/3/movie/550?api_key=f6fc39d1a09cfead5379199d398c2281
-const url = `${BASE_URL}${movieId}?api_key=${API_KEY}&${language}`
 
-console.log(movieId)
-console.log(url)
+function findMovieFunc() {
+  let movieId = Math.floor(Math.random() * 1000 + 1)
+  let url = `${BASE_URL}${movieId}?api_key=${API_KEY}&${language}`
 
-function changeMovie() {
+  console.log(movieId)
+  console.log(url)
+
   axios
     .get(url)
-    .then(response => {
-      console.log(response.overview)
+    .then(res => {
+      const data = res.data
+      console.log(data)
+      movieSummary.textContent = `${data.overview}`
+      movieTitle.textContent = `${data.title}`
+      movieImage.src = `${IMG_URL}${data.poster_path}`
+      movieImage.alt = `Imagem do filme`
     })
-    .catch(error => console.log(error))
+    .catch(error => {
+      movieSummary.textContent = `Bora codar!!`
+      movieTitle.textContent = `Ops, hoje não é dia de assistir filme.`
+      movieImage.src = ``
+      movieImage.alt = `Imagem de erro`
+    })
 }
